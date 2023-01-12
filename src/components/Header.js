5e689da8422053cb2e835dd322d7a710663a8de2
MadZeke7 <erenji@deloitte.com>
2023-01-12 09:58:53 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+function Header(){
+
+    return(
+        <header>
+            <div className='leftNav '>
+                <img src="https://www.azirish.org/wp-content/uploads/2016/05/bookgroup2.png" width="65" height="65"></img>
+                <span>KameHameHa</span>
+            </div>
+            <div>
+                <ul className='rightNav'>
+                <li>Home</li>
+                <li>Login</li>
+                <li>Sign Up</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Books</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
